360752d71e528257b30c5d8c24e896f4a1d16b7f
William Walker <w_walker@icloud.com>
2022-04-26 08:26:54 -0500

    complete 'set_mode'

--- C_Programming/set_mode/TestCode.c
+++ C_Programming/set_mode/TestCode.c
@@ -10,26 +10,28 @@ int getMode(int *array, int size)
     if (array == NULL)
         return -2;
 
-    int max = array[0];
- 
-    for (int i = 1; i < size; i++)
-        if (array[i] > max)
-            max = array[i];
-    
-    int result[size];
-    memset(result, 0, size * sizeof(int));
+    int result[size + 1];
+    memset(result, 0, (size + 1) * sizeof(int));
 
     if (result == NULL)
         return -3;
 
     for (int i = 0; i < size; i++) {
-        result[array[i]]++;
+        int num = *(array + i);
+        *(result + num) += 1;
     }
 
     int mode = -1;
+    int modeCount = 0;
     for (int i = 0; i < size; i++) {
-        mode = (result[i] > mode) ? i : mode;
+        if (*(result + i) > mode) {
+            mode = i;
+            modeCount = *(result + i);
+        }
     }
+
+    if (modeCount == 1)
+        return -1;
 
     return mode;
 }
