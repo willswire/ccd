2749b72a6e6463e9918eb1e87f31a639ddddf1cd
William Walker <w_walker@icloud.com>
2022-05-02 08:25:20 -0500

    complete 'Encrypted_Data'

--- Networking/Encrypted_Data/client.py
+++ Networking/Encrypted_Data/client.py
@@ -14,48 +14,46 @@ cmd_list = {
     'error' : 0x8FF
 }
 
-def pack(cmd, msgLength = 0):
-    padLength = 0
-    if msgLength < 44:
-        padLength = 44 - msgLength
-    else:
-        padLength = 100 - msgLength
-    formatStr = ">I" + str(padLength) + "x"
-    packet = struct.pack(formatStr, cmd_list[cmd])
-    return packet
-
 def enc(msg, key):
     f = Fernet(key)
     return f.encrypt(msg.encode('utf-8'))
+
+def dec(msg, key):
+    f = Fernet(key)
+    return f.decrypt(msg)
 
 #This function should return the server's final message as an actual Python 3 string,
 #not as a byte sequence.
 def get_message_using_encrypted_request_protocol():
     s = socket.socket()
     host = socket.gethostname()
-    port = 12345
+    port = 7777
     s.bind((host, port))
     s.connect(dest)
 
     ## 1
-    message = pack('key-request')
+    message = struct.pack(">I44x", cmd_list['key-request'])
     print("Sending message: ", message)
     s.send(message)
 
     ## 2
-    message = s.recv(100)
+    message = s.recv(48)
     print("Message received: ", message)
     key = message[4:]
 
     ## 3
     payload = enc("I challenge!", key)
-    print("Encrypted payload: ", payload)
-    cmd = socket.htonl(cmd_list['encrypted-message'])
-    message = cmd + payload
+    message = struct.pack(">I", cmd_list['encrypted-message']) + payload
+    print("Sending message: ", message)
     s.send(message)
 
+    ## 4
+    message = s.recv(104)
+    print("Message received: ", message)
+    text = dec(message, key).decode('utf-8')
     s.close()
-    return ""
+
+    return text
 
 
 if __name__ == "__main__":
